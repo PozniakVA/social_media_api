8410367a005d15412dbea1dea09145642d37a2fd
Valentin Pozniak <waliker448@gmail.com>
2024-09-13 17:53:20 +0300

    implement media file

--- social_media/models.py
+++ social_media/models.py
@@ -1,6 +1,22 @@
+import pathlib
+import uuid
+from typing import Any
+
 from django.db import models
+from django.utils.text import slugify
 
 from social_media_api import settings
+
+def image_path(instance: Any, filename: str) -> pathlib.Path:
+    filename = f"{slugify(instance.info)}-{uuid.uuid4()}" + pathlib.Path(filename).suffix
+
+    path_at_media = "upload/"
+    if isinstance(instance, Post):
+        path_at_media = "upload/post/"
+    elif isinstance(instance, Profile):
+        path_at_media = "upload/profile_image/"
+
+    return pathlib.Path(path_at_media) / pathlib.Path(filename)
 
 
 class Profile(models.Model):
@@ -10,7 +26,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE
     )
     bio = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField(blank=True, null=True)
+    profile_image = models.ImageField(null=True, upload_to=image_path)
     posts = models.ManyToManyField("Post", blank=True)
 
     def __str__(self) -> str:
@@ -29,7 +45,7 @@ class Follow(models.Model):
 class Post(models.Model):
     name = models.CharField(max_length=100)
     text = models.TextField(blank=True, null=True)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(null=True, upload_to=image_path)
     hashtag = models.ManyToManyField(
         "Hashtag",
         related_name="posts",
