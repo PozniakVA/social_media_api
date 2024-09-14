527d262051ace46722074f6b12ec9aa4a2a97d85
Valentin Pozniak <waliker448@gmail.com>
2024-09-14 08:53:37 +0300

    add router for Profile, Follow, Post, Hashtag views

---
+++ social_media/urls.py
@@ -0,0 +1,17 @@
+from rest_framework import routers
+
+from social_media.views import (
+    ProfileViewSet,
+    FollowViewSet,
+    PostViewSet,
+    HashtagViewSet
+)
+
+router = routers.SimpleRouter()
+router.register("profile", ProfileViewSet)
+router.register("follow", FollowViewSet)
+router.register("posts", PostViewSet)
+router.register("hashtags", HashtagViewSet)
+urlpatterns = router.urls
+
+app_name = "social_media"
