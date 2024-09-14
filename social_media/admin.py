76a801fcc8dd98995ae5bba6e83e837c6b6f4c52
Valentin Pozniak <waliker448@gmail.com>
2024-09-14 08:59:26 +0300

    register models in admin.py

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import (
+    Profile,
+    Post,
+    Follow,
+    Hashtag
+)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    pass
