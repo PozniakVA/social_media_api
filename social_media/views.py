f71e1b88dfade0a2d7b8443c28b713abd645faa4
Valentin Pozniak <waliker448@gmail.com>
2024-09-14 08:10:55 +0300

    add default views for Hashtag, Post, Follow, Profile model

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from social_media.models import Profile, Hashtag, Post, Follow
+from social_media.serializer import ProfileSerializer, HashtagSerializer, PostSerializer, FollowSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class HashtagViewSet(viewsets.ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
